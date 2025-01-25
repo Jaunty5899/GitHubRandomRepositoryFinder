a248c218f28a8d24a24b414d4fd5046932af3939
Jaunty <jaunty5899@gmail.com>
2025-01-25 15:10:11 +0530

    50% done

---
+++ src/Dropdown.jsx
@@ -0,0 +1,28 @@
+import "./Dropdown.css";
+import { useState } from "react";
+
+export default function Dropdown({ Data }) {
+  const [toggle, setToggle] = useState(false);
+  const [choice, setChoice] = useState(Data[0].title);
+  return (
+    <div className="dropDownContainer">
+      <div className="dropDownButton" onClick={() => setToggle(true)}>
+        {choice}
+        <span className="dropDownDirection">{toggle ? "▲" : "▼"}</span>
+      </div>
+      <div className={`dropDownList ${toggle && "active"}`}>
+        {Data.map((e) => (
+          <div
+            className={`dropDownOption ${e.title == choice && "activeMarker"}`}
+            onClick={() => {
+              setChoice(e.title);
+              setToggle(false);
+            }}
+          >
+            {e.title}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
